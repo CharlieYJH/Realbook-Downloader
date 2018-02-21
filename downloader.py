11ab530ec531423df6ed99776c4cda626f01059f
Charlie Yin <charlieyinjunhao@hotmail.com>
2018-02-22 00:42:38 +0800

    Now takes into account the fact that some sheets may have two pages

--- downloader.py
+++ downloader.py
@@ -2,12 +2,6 @@ import os
 from robobrowser import RoboBrowser
 from string import capwords
 
-def check_filename(directory, name):
-    for f in os.listdir(os.path.dirname(directory)):
-        if os.path.splitext(f)[0] == name:
-            return True
-    return False
-
 url = "http://www.guitarcats.com/realbook-jazz-standards/A"
 
 # Open up HTML
@@ -36,45 +30,48 @@ for link in links:
     save_path = "./Lead Sheets/" + letter + "/"
     song_link_list = browser.find_all("li", {"class": "realbooks_song_li"})
 
-    # Make a folder for the current category
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
     for song_link in song_link_list:
 
         song_name = capwords(song_link.text)
+        filepath = save_path + song_name + "/"
 
-        # Check if this song is already saved
-        if (check_filename(save_path, song_name)):
+        if os.path.isdir(filepath):
             print("SKIPPED: " + song_name)
             continue
+        else:
+            os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
         browser.open(song_link.find("a")["href"])
 
         # Find the div containing the lead sheet
-        song_img_div = browser.find("div", {"class": "realbook_song_div"})
+        song_img_divs = browser.find_all("div", {"class": "realbook_song_div"})
 
-        # Skip if it doesn't exist
-        if not song_img_div:
-            continue
+        multiple_pages = True if len(song_img_divs) > 1 else False
+        counter = 1
 
-        # Get the image src and its extension
-        song_img_src = song_img_div.find("img")["src"]
-        file_ext = song_img_src.split(".")[-1]
+        for song_img_div in song_img_divs:
 
-        filename = save_path + song_name + "." + file_ext
+            # Skip if it doesn't exist
+            if not song_img_div:
+                continue
 
-        #  if os.path.isfile(filename):
-            #  print("SKIPPED: " + song_name)
-            #  continue
+            # Get the image src and its extension
+            song_img_src = song_img_div.find("img")["src"]
+            file_ext = song_img_src.split(".")[-1]
 
-        browser.open(song_img_src)
+            page_num = " " + str(counter) if multiple_pages else ""
+            filename = song_name + page_num + "." + file_ext
 
-        # Check proper response
-        if browser.response.status_code != 200:
-            print("Server responded with code " + str(browser.response.status_code) + " for " + song_img_src)
-            continue
+            counter = counter + 1;
 
-        # Write the image onto disk
-        with open(save_path + song_name + "." + file_ext, "wb") as output:
-            output.write(browser.response.content)
-            print("SAVED: " + song_name)
+            browser.open(song_img_src)
+
+            # Check proper response
+            if browser.response.status_code != 200:
+                print("Server responded with code " + str(browser.response.status_code) + " for " + song_img_src)
+                continue
+
+            # Write the image onto disk
+            with open(filepath + filename, "wb") as output:
+                output.write(browser.response.content)
+                print("SAVED: " + filename)
